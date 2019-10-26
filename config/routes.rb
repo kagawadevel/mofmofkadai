858393efc0188ab9ddb81913e5c66e4bcb908c0e
kakegawa <cmuwni@gmail.com>
2019-10-26 20:29:10 +0900

    propertys_controllerと各viewファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'propertys/index'
+
+  get 'propertys/show'
+
+  get 'propertys/new'
+
+  get 'propertys/edit'
+
+  get 'propertys/create'
+
+  get 'propertys/update'
+
+  get 'propertys/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
