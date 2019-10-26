567a20e0402b51279a6f66a9655f09888f0b536e
kakegawa <cmuwni@gmail.com>
2019-10-26 20:18:15 +0900

    propertyモデルにバリデーション追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 50}
+  validates :rent, presence: true, numericality: { only_integer: true}
+  validates :address, presence: true, length: { maximum: 100}
+  validates :age, presence: true, numericality: { only_integer: true }
+  validates :remarks, presence: true, length: { maximum: 150}
 end
