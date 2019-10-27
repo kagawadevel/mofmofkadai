12da23c635063bbe42175bd33d8bbd47be495b36
kakegawa <cmuwni@gmail.com>
2019-10-27 20:03:52 +0900

    stationモデルのバリデーションを削除

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,8 @@
 class Property < ApplicationRecord
 
   has_many :stations, dependent: :destroy
+  accept_nested_attributes_for :stations
+
   validates :name, presence: true, length: { maximum: 50}
   validates :rent, presence: true, numericality: { only_integer: true}
   validates :address, presence: true, length: { maximum: 100}
